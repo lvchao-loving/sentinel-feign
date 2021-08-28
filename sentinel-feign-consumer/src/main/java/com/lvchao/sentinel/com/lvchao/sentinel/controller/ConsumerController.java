fb525e8237995b17838de44cdb00483947b35935
lvchao <532180467@qq.com>
2021-08-28 18:25:47 +0800

    初始化项目3

--- sentinel-feign-consumer/src/main/java/com/lvchao/sentinel/com/lvchao/sentinel/controller/ConsumerController.java
+++ sentinel-feign-consumer/src/main/java/com/lvchao/sentinel/com/lvchao/sentinel/controller/ConsumerController.java
@@ -1,6 +1,9 @@
 package com.lvchao.sentinel.com.lvchao.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.lvchao.sentinel.api.ProviderClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import java.util.Random;
  * @version: V1.0
  */
 @RestController
+@Slf4j
 public class ConsumerController {
 
     @Value("${spring.application.name}")
@@ -29,5 +33,17 @@ public class ConsumerController {
     public String getConsumer(){
         Random random = new Random();
         return applicationName + ":" + random.nextInt(1000) + ":拼接:" + providerClient.getProvicer();
+    }
+
+    //@SentinelResource(value = "sentinel_springcloud",blockHandler = "exceptionHandler")
+    @GetMapping("/consumer/getMyConsumer")
+    public String getMyConsumer(){
+        Random random = new Random();
+        return applicationName + ":" + random.nextInt(1000) ;
+    }
+
+    public String exceptionHandler(BlockException e){
+        log.info( "系统繁忙，请稍等！" + e);
+        return "系统繁忙，请稍等！";
     }
 }
